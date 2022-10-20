8b7b90376fe74dafce6dcec530fe2ebc06c8d527
Nebby2025 <m251446@usna.edu>
2022-10-20 14:59:53 -0400

    10/20/2022
    1459

--- game.py
+++ game.py
@@ -21,17 +21,21 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            self._check_events()
+            self._update_screen()
             #Watch for keyboard inputs and mouse movement
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+    def _check_events(self):
+        """Respond to inputs and mouse movement."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Make a game instance, and run the game
